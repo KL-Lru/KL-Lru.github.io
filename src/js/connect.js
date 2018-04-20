d0f40745855726e633281a32211fa1463c9fefc9
KL-Lru <kilattoeruru@outlook.jp>
2018-04-21 04:50:24 +0900

    ver1.08

--- src/js/connect.js
+++ src/js/connect.js
@@ -5,6 +5,8 @@ $(document).ready(function(){
   $("#nlp").load("/src/html/nlp.html",function(){
     $("#nlp_top").load("/src/html/nlp/top.html");
     $("#chapter1").load("/src/html/nlp/chapter1.html");
-    $("#chapter2").load("/src/html/nlp/chapter2.html");
+    $("#chapter2").load("/src/html/nlp/chapter2.html",function(){
+      Prism.highlightAll();
+    });
   });
 });
