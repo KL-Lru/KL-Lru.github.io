c2ca01501a8394b7e8721cb97b4ec30945c1bbc5
KL-Lru <kilattoeruru@outlook.jp>
2018-04-21 04:31:32 +0900

    ver1.05

--- src/js/nlp_connect.js
+++ src/js/nlp_connect.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).ready(function(){
   $("#nlp_top").load("/src/html/nlp/top.html");
   $("#chapter1").load("/src/html/nlp/chapter1.html");
   $("#chapter2").load("/src/html/nlp/chapter2.html");
