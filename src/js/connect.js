2cf4375b833431069990e391c300e4601b7203e6
KL-Lru <kilattoeruru@outlook.jp>
2018-04-21 04:47:34 +0900

    ver1.07

--- src/js/connect.js
+++ src/js/connect.js
@@ -2,5 +2,9 @@ $(document).ready(function(){
   $("#navigater").load("/src/html/navi.html");
   $("#top").load("/src/html/top.html");
   $("#profile").load("/src/html/profile.html");
-  $("#nlp").load("/src/html/nlp.html");
+  $("#nlp").load("/src/html/nlp.html",function(){
+    $("#nlp_top").load("/src/html/nlp/top.html");
+    $("#chapter1").load("/src/html/nlp/chapter1.html");
+    $("#chapter2").load("/src/html/nlp/chapter2.html");
+  });
 });
