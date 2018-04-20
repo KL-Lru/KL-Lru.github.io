254515322e5d62c07e4d544120aff7e91f78cd6a
KL-Lru <kilattoeruru@outlook.jp>
2018-04-21 04:22:40 +0900

    ver1.02

--- src/js/connect.js
+++ src/js/connect.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
-  $("#navigater").load("./src/html/navi.html");
-  $("#top").load("./src/html/top.html");
-  $("#profile").load("./src/html/profile.html");
-  $("#nlp").load("./src/html/nlp.html");
+  $("#navigater").load("/src/html/navi.html");
+  $("#top").load("/src/html/top.html");
+  $("#profile").load("/src/html/profile.html");
+  $("#nlp").load("/src/html/nlp.html");
 });
