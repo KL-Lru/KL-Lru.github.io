314a21eba980dcbb7cf73bab39a7427123d4f617
KL-Lru <kilattoeruru@outlook.jp>
2018-04-21 14:29:25 +0900

    test

--- src/js/connect.js
+++ src/js/connect.js
@@ -2,11 +2,10 @@ $(document).ready(function(){
   $("#navigater").load("/src/html/navi.html");
   $("#top").load("/src/html/top.html");
   $("#profile").load("/src/html/profile.html");
+  $("#blog").load("/src/html/blog.html");
   $("#nlp").load("/src/html/nlp.html",function(){
     $("#nlp_top").load("/src/html/nlp/top.html");
     $("#chapter1").load("/src/html/nlp/chapter1.html");
     $("#chapter2").load("/src/html/nlp/chapter2.html");
   });
-  $("#blog").load("/src/html/blog.html");
-  Prism.highlightElement(document.getElementsById('test_code'));
 });
