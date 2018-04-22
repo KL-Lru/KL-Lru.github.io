2b452da2c99133547a04f0f17172b161427f05dd
KL-Lru <kilattoeruru@outlook.jp>
2018-04-22 19:32:06 +0900

    test

--- src/js/connect.js
+++ src/js/connect.js
@@ -1,21 +1,17 @@
-function move(st){
-  location.href = "/src/html/"+st+".html";
-};
-
-$(function(){
-  $(".connect").on("click",function(){
-    event.preventDefault();
-    event.stopPropagation();
-      $('document').animate(
-        {opacity: '0'},
-        {duration: 'slow',
-        easing: 'swing',
-        complete: move($(this).attr('value'))
-        }
-    );
-    $('document').animate(
-        {opacity: '1',},
-        {duration: 'slow'});
+document.addEventListener("DOMContentLoaded",function(){
+  $("#navigater").load("/src/html/navi.html");
+  $("#top").load("/src/html/top.html");
+  $("#profile").load("/src/html/profile.html");
+  $("#blog").load("/src/html/blog.html");
+  $("#nlp").load("/src/html/nlp.html",function(){
+    $("#nlp_top").load("/src/html/nlp/top.html");
+    $("#chapter1").load("/src/html/nlp/chapter1.html",function(){
+      self.Prism.fileHighlight();
+    });
+    $("#chapter2").load("/src/html/nlp/chapter2.html",function(){
+      self.Prism.fileHighlight();
+    });
   });
+  $("#atcoder").load("/src/html/atcoder.html");
 });
 
--- src/js/loader.js
+++
@@ -1,3 +0,0 @@
-document.addEventListener("DOMContentLoaded",function(){
-  $("#navigater").load("/src/html/navi.html");
-});
