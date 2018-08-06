1112101b21a9f6520a84be29673deaa69fc64735
KL-Lru <kilattoeruru@outlook.jp>
2018-08-07 04:14:40 +0900

    ver.2.81

--- src/js/connector/top.js
+++ src/js/connector/top.js
@@ -2,5 +2,7 @@ document.addEventListener("DOMContentLoaded",function(){
   $("#navigater").load("/src/html/navi.html",function(){
     $("#version").load("/version");
   });
-  $("#top").load("/src/html/top.html");
+  $("#top").load("/src/html/top.html",function(){
+    $("#history").load("/src/html/history.html")
+  });
 });
