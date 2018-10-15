4897a5d465b9a634fa9d7fcdbc7085329ab8a677
KL-Lru <kilattoeruru@outlook.jp>
2018-10-16 00:35:24 +0900

    math-test

--- src/js/connector/algorithm.js
+++ src/js/connector/algorithm.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded",function(){
   if (arg){
     $("#algorithm").load("/src/html/comp/" + arg + ".html", function(){
       self.Prism.highlightAll();
+      MathJax.Hub.Queue();
     });
   }else{
     $("#algorithm").load("/src/html/algorithm.html", function(){
